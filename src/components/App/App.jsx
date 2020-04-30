61f26a6c007bc5b84e679dcd68e1260d6bc0f8e6
Chelsea Spangler <chelseanspangler@gmail.com>
2020-04-30 16:40:43 -0700

    Form component added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import Header from '../Header/Header.jsx';
+import Form from '../Form/Form.jsx';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Header />
+      <Form />
+    </>
+  );
 }
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Form = () => (
+  <form>
+    <input type="text" id="URL" placeholder="URL" />
+    <label htmlFor="GET">GET</label>
+    <input type="radio" name="HTTP" id="GET" value="GET" />
+    <label htmlFor="POST">GET</label>
+    <input type="radio" name="HTTP" id="POST" value="POST" />
+    <label htmlFor="GET">PUT</label>
+    <input type="radio" name="HTTP" id="PUT" value="PUT" /> 
+    <label htmlFor="GET">PATCH</label>
+    <input type="radio" name="HTTP" id="PATCH" value="PATCH" />
+    <label htmlFor="GET">DELETE</label>
+    <input type="radio" name="HTTP" id="DELETE" value="DELETE" />
+    <button>Go!</button>
+  </form>
+);
+
+export default Form;
