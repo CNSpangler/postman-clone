569cfacdc6937c54c395eb37a01fd65f33925c6f
Chelsea Spangler <chelseanspangler@gmail.com>
2020-04-30 16:54:32 -0700

    passing snapshot test for Form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -5,7 +5,7 @@ const Form = () => (
     <input type="text" id="URL" placeholder="URL" />
     <label htmlFor="GET">GET</label>
     <input type="radio" name="HTTP" id="GET" value="GET" />
-    <label htmlFor="POST">GET</label>
+    <label htmlFor="POST">POST</label>
     <input type="radio" name="HTTP" id="POST" value="POST" />
     <label htmlFor="GET">PUT</label>
     <input type="radio" name="HTTP" id="PUT" value="PUT" /> 
@@ -13,6 +13,7 @@ const Form = () => (
     <input type="radio" name="HTTP" id="PATCH" value="PATCH" />
     <label htmlFor="GET">DELETE</label>
     <input type="radio" name="HTTP" id="DELETE" value="DELETE" />
+    <input type="textarea" name="body" placeholder="Raw JSON Body" />
     <button>Go!</button>
   </form>
 );
---
+++ src/components/Form/Form.test.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Form from './Form.jsx';
+
+describe('Form component', () => {
+  it('renders Form ', () => {
+    const wrapper = shallow(<Form />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
