6d62d7c613885cb5d6425b47652ddfc5ac9fe643
Chelsea Spangler <chelseanspangler@gmail.com>
2020-05-03 13:18:54 -0700

    add grid for Form elements

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -19,26 +19,29 @@ const RadioGroup = ({ name, onChange, children }) => {
 };
 
 const RadioButton = ({ name, value, onChange }) => (
-  <>
+  <div className={styles.radioButton} >
     <input id={value} type="radio" name={name} value={value} onChange={onChange} />
     <label htmlFor={value}>{value}</label>
-  </>
+  </div>
 );
 
 /* eslint-enable react/prop-types */
 const Form = ({ url, onUrlChange, body, onBodyChange, onMethodChange, onSubmit }) => (
-  <form onSubmit={onSubmit} className={styles.Form}>
-    <input type="text" value={url} placeholder="URL" onChange={onUrlChange} />
-    <RadioGroup name="method" onChange={onMethodChange} classname={styles.radioGroup}>
-      <RadioButton value="POST" />
-      <RadioButton value="GET" />
-      <RadioButton value="PATCH" />
-      <RadioButton value="PUT" />
-      <RadioButton value="DELETE" />
-    </RadioGroup>
-    <input type="textarea" value={body} placeholder="Raw JSON Body" onChange={onBodyChange} />
-    <button>Submit</button>
-  </form>
+  <div className={styles.Form}>
+    <form onSubmit={onSubmit} className={styles.formGrid}>
+      <input type="text" value={url} placeholder="URL" onChange={onUrlChange} className={styles.url}/>
+      <RadioGroup name="method" onChange={onMethodChange} classname={styles.radioGroup}>
+        <RadioButton value="POST" />
+        <RadioButton value="GET" />
+        <RadioButton value="PATCH" />
+        <RadioButton value="PUT" />
+        <RadioButton value="DELETE" />
+      </RadioGroup>
+      <input type="textarea" value={body} placeholder="Raw JSON Body" onChange={onBodyChange} className={styles.body} />
+      <div className={styles.headers} />
+      <button className={styles.button}>Submit</button>
+    </form>
+  </div>
 );
 
 Form.propTypes = {
